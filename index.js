a7cc52288e486c370bedeee708f6e91ec2a6ec02
Hafidz Mahrus <havus.it@gmail.com>
2024-07-26 00:44:02 +0700

    init server

---
+++ index.js
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+
+
+// Import the framework and instantiate it
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+
+const fastify = Fastify({
+  logger: true,
+});
+
+await fastify.register(cors, { 
+  origin: '*',
+  methods: ['GET'],
+})
+
+// Declare a route
+fastify.get('/ping', async function handler (request, reply) {
+  return { status: 'WELL DONE' };
+});
+fastify.get('/hello-world', async function handler (request, reply) {
+  return { message: 'Hello world!' };
+});
+
+// Run the server!
+try {
+  await fastify.listen({ port: process.env.PORT });
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1);
+}
